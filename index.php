95c346b04e64ecbeb14aa4a90f270128916c6de1
KevinPann <36241579+KevinPann@users.noreply.github.com>
2021-02-22 16:02:59 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,42 @@
 <?php
 
-require("index.view.php");
+//require 'function.php';
+
+//todo Application
+//
+//todo, comment, User
+
+class Task
+{
+
+    public $description;
+    protected $completed = false;
+
+
+    public function __construct($description)
+    {
+        //Automatically triggered on instantiation
+        $this->description = $description;
+    }
+
+    public function complete()
+    {
+        $this->completed = true;
+    }
+
+    public function isComplete()
+    {
+        return $this->completed;
+    }
+}
+$tasks = [
+    new Task('Fly'),
+    new Task('Walk'),
+    new Task('Eat'),
+];
+
+$tasks[0]->complete();
+
+// $task->complete();
+
+require 'index.view.php';
